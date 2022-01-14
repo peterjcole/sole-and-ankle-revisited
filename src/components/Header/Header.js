04919ea42b4d6b1bcc7fb1f67d2fabea1d4853d0
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2022-01-14 14:37:03 +0000

    exercises 1 and 2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import MobileMenu from '../MobileMenu';
+import { COLORS, QUERIES, WEIGHTS } from '../../constants'
+import Logo from '../Logo'
+import SuperHeader from '../SuperHeader'
+import MobileMenu from '../MobileMenu'
+import Icon from '../Icon'
 
 const Header = () => {
-  const [showMobileMenu, setShowMobileMenu] = React.useState(false);
+  const [showMobileMenu, setShowMobileMenu] = React.useState(false)
 
   // For our mobile hamburger menu, we'll want to use a button
   // with an onClick handler, something like this:
@@ -21,15 +22,26 @@ const Header = () => {
         <Side>
           <Logo />
         </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
+        <DesktopNav>
+          <NavLink href='/sale'>Sale</NavLink>
+          <NavLink href='/new'>New&nbsp;Releases</NavLink>
+          <NavLink href='/men'>Men</NavLink>
+          <NavLink href='/women'>Women</NavLink>
+          <NavLink href='/kids'>Kids</NavLink>
+          <NavLink href='/collections'>Collections</NavLink>
+        </DesktopNav>
+        <TabletAndBelowNav>
+          <NavLink>
+            <Icon id='shopping-bag' />
+          </NavLink>
+          <NavLink>
+            <Icon id='search' />
+          </NavLink>
+          <NavLink>
+            <Icon id='menu' />
+          </NavLink>
+        </TabletAndBelowNav>
+        <DesktopOnlySide />
       </MainHeader>
 
       <MobileMenu
@@ -37,26 +49,61 @@ const Header = () => {
         onDismiss={() => setShowMobileMenu(false)}
       />
     </header>
-  );
-};
+  )
+}
 
 const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
-  padding: 18px 32px;
+  padding: 16px clamp(
+          16px,
+          4vw,
+          32px
+  );
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
-`;
 
-const Nav = styled.nav`
+  @media ${QUERIES.tabletAndBelow} {
+    //justify-content: space-between;
+    align-items: center;
+  }
+
+`
+
+const DesktopNav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
-`;
+
+  @media ${QUERIES.tabletAndBelow} {
+    display: none;
+  }
+`
+
+const TabletAndBelowNav = styled.nav`
+  display: none;
+  
+  @media ${QUERIES.tabletAndBelow} {
+    display: flex;
+    align-items: center;
+
+    gap: clamp(
+            24px,
+            20vw,
+            48px
+    )
+  }
+`
 
 const Side = styled.div`
   flex: 1;
-`;
+`
+
+const DesktopOnlySide = styled(Side)`
+  @media ${QUERIES.tabletAndBelow} {
+    display: none;
+  }
+`
 
 const NavLink = styled.a`
   font-size: 1.125rem;
@@ -68,6 +115,6 @@ const NavLink = styled.a`
   &:first-of-type {
     color: ${COLORS.secondary};
   }
-`;
+`
 
-export default Header;
+export default Header
--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,15 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+export const BREAKPOINTS = {
+  phoneMax: 600,
+  tabletMax: 950,
+  laptopMax: 1300,
+}
+
+export const QUERIES = {
+  'phoneAndBelow': `(max-width: ${BREAKPOINTS.phoneMax}px)`,
+  'tabletAndBelow': `(max-width: ${BREAKPOINTS.tabletMax}px)`,
+  'laptopAndBelow': `(max-width: ${BREAKPOINTS.laptopMax}px)`,
+}
